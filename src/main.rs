8f144f0d1ed8e3cba73f16debfba244dbafe0e97
nabetama <mao.nabeta@gmail.com>
2023-02-07 21:12:11 +0900

    read csv that written Shift-JIS

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use clap::Parser;
+use std::error::Error;
+use std::fs;
+
+#[derive(Debug, Parser)]
+struct Cli {
+    file: String,
+}
+
+fn read_shift_jis_csv(path: &str) -> Result<(), Box<dyn Error>> {
+    // 一度 Vec で読み込む
+    let file = fs::read(path)?;
+    // SHIFT_JIS を decode -> utf8 にencodeする
+    let (res, _, _) = encoding_rs::SHIFT_JIS.decode(&file);
+
+    let mut rdr = csv::Reader::from_reader(res.as_bytes());
+    for result in rdr.records() {
+        let record = result?;
+        println!("{:?}", record);
+    }
+
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+
+    match read_shift_jis_csv(&args.file) {
+        Ok(_) => {
+            println!("done");
+        }
+        Err(err) => {
+            println!("abnormal exit, {:?}", err.to_string())
+        }
+    }
 }
