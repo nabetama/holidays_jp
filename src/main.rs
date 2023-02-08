b2aa816e2193063d3607bf171711b7f076000165
nabetama <mao.nabeta@gmail.com>
2023-02-08 14:00:08 +0900

    read csv

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use chrono::NaiveDate;
 use clap::Parser;
 use std::error::Error;
 use std::fs;
@@ -7,27 +8,43 @@ struct Cli {
     file: String,
 }
 
-fn read_shift_jis_csv(path: &str) -> Result<(), Box<dyn Error>> {
+#[derive(Debug)]
+#[allow(dead_code)]
+struct Holiday {
+    date: NaiveDate,
+    name: String,
+}
+
+fn read_shift_jis_csv(path: &str) -> Result<Vec<Holiday>, Box<dyn Error>> {
     // 一度 Vec で読み込む
     let file = fs::read(path)?;
     // SHIFT_JIS を decode -> utf8 にencodeする
     let (res, _, _) = encoding_rs::SHIFT_JIS.decode(&file);
-
     let mut rdr = csv::Reader::from_reader(res.as_bytes());
-    for result in rdr.records() {
-        let record = result?;
-        println!("{:?}", record);
+
+    let mut holidays: Vec<Holiday> = Vec::new();
+    for record in rdr.records() {
+        let record = record?;
+
+        let holiday = Holiday {
+            date: NaiveDate::parse_from_str(&String::from(&record[0]), "%Y/%m/%d")?,
+            name: String::from(&record[1]),
+        };
+
+        holidays.push(holiday);
     }
 
-    Ok(())
+    Ok(holidays)
 }
 
 fn main() {
     let args = Cli::parse();
 
     match read_shift_jis_csv(&args.file) {
-        Ok(_) => {
-            println!("done");
+        Ok(holidays) => {
+            for holiday in holidays {
+                println!("{:?}", holiday);
+            }
         }
         Err(err) => {
             println!("abnormal exit, {:?}", err.to_string())
