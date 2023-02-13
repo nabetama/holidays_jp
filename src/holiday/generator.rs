ff329f9c4e3bbed8fcd49ed22305b5afc6171b20
nabetama <mao.nabeta@gmail.com>
2023-02-14 00:44:43 +0900

    delete comments

--- src/holiday/generator.rs
+++ src/holiday/generator.rs
@@ -25,16 +25,13 @@ mod tests {
 #[warn(dead_code)]
 #[tokio::main]
 pub async fn generate() -> Result<()> {
-    // csv ファイルをダウンロードする
     let body = reqwest::get("https://www8.cao.go.jp/chosei/shukujitsu/syukujitsu.csv")
         .await?
         .text_with_charset("shift-jis")
         .await?;
 
-    // csv 読み込む（csv reader そのまま使う）
     let mut rdr = csv::Reader::from_reader(body.as_bytes());
 
-    // ファイルの先頭
     let header = "use std::collections::HashMap;
     pub fn dates() -> HashMap<String, String> {
         let res = HashMap::from([    
@@ -44,18 +41,10 @@ pub async fn generate() -> Result<()> {
     res
 }";
 
-    // csv から書き込む
     let path = Path::new("./src/holiday/dates.rs");
-
-    println!("{:?}", path);
-
-    // ファイルを書き込みモードで開く
     let mut writer = BufWriter::new(File::create(&path)?);
 
-    // header 書き込む
     write!(&mut writer, "{}", header)?;
-
-    // まんなか書き込む
     for record in rdr.records() {
         let record = record?;
 
@@ -67,11 +56,7 @@ pub async fn generate() -> Result<()> {
             &record[1]
         )?;
     }
-
-    // footer 書き込む
     write!(&mut writer, "{}", footer)?;
-
-    // flush する
     writer.flush()?;
 
     Ok(())
