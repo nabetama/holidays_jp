8113af0dc3c2bd7cdbecbab5668b96f64709e619
nabetama <mao.nabeta@gmail.com>
2023-02-16 11:48:44 +0900

    add comment and some refactoring

--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,17 @@ use crate::holiday::generator::generate;
 const CSV_FILE_URL: &str = "https://www8.cao.go.jp/chosei/shukujitsu/syukujitsu.csv";
 const OUT_FILE: &str = "./src/holiday/dates.rs";
 
+/// A struct with command line arguments for CLI
+///
+/// # Example
+///
+/// ```no_run
+/// let opt = CliOption { date: "2023/01/01", gen: true };
+/// ```
 #[derive(Debug)]
 pub struct CliOption {
     date: String,
+    gen: bool,
 }
 
 fn main() -> Result<()> {
@@ -42,17 +50,17 @@ fn main() -> Result<()> {
         )
         .get_matches();
 
-    match matches.get_one::<bool>("gen") {
-        Some(_) => {
-            generate(CSV_FILE_URL, OUT_FILE)?;
-            println!("generate process is done");
-            process::exit(0x0100);
-        }
-        None => {}
+    let date = get_date(matches.get_one::<String>("date").unwrap())?;
+    let gen = matches.get_one::<bool>("gen").is_some();
+
+    let opt = CliOption { date, gen };
+
+    if opt.gen {
+        generate(CSV_FILE_URL, OUT_FILE)?;
+        println!("generate process is done");
+        process::exit(0x0100);
     }
 
-    let date = get_date(matches.get_one::<String>("date").unwrap())?;
-    let opt = CliOption { date };
     let holidays = dates::dates();
 
     match find_holiday(holidays, opt, &mut std::io::stdout()) {
